c5516763198eb4e5718c619364f4f0e51c2b7869
Sidharth Vaitha <vaitha2@illinois.edu>
2017-11-07 12:36:17 -0600

    first commit

--- mp3/digit.py
+++ mp3/digit.py
@@ -14,25 +14,25 @@ def readtraindata():
 	with open('digitdata/traininglabels') as f:
 	    trainlabels = f.readlines()
 	trainlabels = [int(x.strip()) for x in trainlabels]
-	for i in range(len(trainlabels)):
+	for x in range(len(trainlabels)):
 		with open('digitdata/trainingimages', 'r') as infile:
 			lines = [line for line in infile][start:end]
-			for i in range(len(lines)):
-				lines[i] = lines[i][:-1]
-			# print(len(lines))
-			# print(len(lines[0]))
-			value = trainlabels[i]
-			count[value] += 1
-			item = data[value]
-			for i in range(len(lines)):
-				line = lines[i]
-				for j in range(len(line)):
-					c = line[j]
-					if (c == '+' or c=='#'):
-						item[i][j] += 1
-			item = data[value]
-			start += 28
-			end += 28
+		for k in range(len(lines)):
+			lines[k] = lines[k][:-1]
+		# print(len(lines))
+		# print(len(lines[0]))
+		value = trainlabels[x]
+		count[value] += 1
+		item = data[value]
+		for i in range(len(lines)):
+			line = lines[i]
+			for j in range(len(line)):
+				c = line[j]
+				if (c == '+' or c=='#'):
+					item[i][j] += 1
+		item = data[value]
+		start += 28
+		end += 28
 
 def main():
 	starttime = time.time()
